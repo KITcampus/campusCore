34dfa02af54632ccf2d08095f1fc112829fc2fc6
jyeong11 <bjy0911@naver.com>
2025-12-05 13:56:18 +0900

    헤더 다 만듬

--- campusCore/Header.xaml.cs
+++ campusCore/Header.xaml.cs
@@ -24,5 +24,10 @@ namespace campusCore
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
