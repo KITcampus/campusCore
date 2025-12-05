d8352a6190dd6b24e344e10808104d089d406d2a
2jw-w <dlwjddn6491@gmail.com>
2025-12-05 16:39:03 +0900

    App 주석

--- campusCore/App.xaml.cs
+++ campusCore/App.xaml.cs
@@ -7,21 +7,21 @@ namespace campusCore
     
     public partial class App : Application
     {
-        private void Application_Startup(object sender, StartupEventArgs e)
-        {
-            LoginWindow login = new LoginWindow();
-            bool? result = login.ShowDialog();
+        //private void Application_Startup(object sender, StartupEventArgs e)
+        //{
+        //    LoginWindow login = new LoginWindow();
+        //    bool? result = login.ShowDialog();
 
-            if (result == true)
-            {
-                MainWindow main = new MainWindow();
-                main.Show();
-            }
-            else
-            {
-                Application.Current.Shutdown();
-            }
-        }
+        //    if (result == true)
+        //    {
+        //        MainWindow main = new MainWindow();
+        //        main.Show();
+        //    }
+        //    else
+        //    {
+        //        Application.Current.Shutdown();
+        //    }
+        //}
     }
 
 }
