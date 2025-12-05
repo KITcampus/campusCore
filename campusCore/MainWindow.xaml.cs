47dce89d71e1b822e2f1ce6514fea7bcad00d63a
jyeong11 <bjy0911@naver.com>
2025-12-05 13:36:44 +0900

    헤더 수정중

--- campusCore/MainWindow.xaml.cs
+++ campusCore/MainWindow.xaml.cs
@@ -20,5 +20,10 @@ namespace campusCore
         {
             InitializeComponent();
         }
+
+        private void Logout_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
