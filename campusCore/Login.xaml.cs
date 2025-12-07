9810c68dab4c21675bde63bf7d53a14716a20945
jyeong11 <bjy0911@naver.com>
2025-12-07 14:42:09 +0900

    로그인 다시 수정

--- campusCore/Login.xaml.cs
+++ campusCore/Login.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using campusCore.Common;
+using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +13,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using System.Data.SQLite;
 
 namespace campusCore
 {
@@ -23,10 +24,32 @@ namespace campusCore
         public Login()
         {
             InitializeComponent();
+            // 저장된 아이디 불러오기
+            if (!string.IsNullOrEmpty(Properties.Settings.Default.SavedId))
+            {
+                txtId.Text = Properties.Settings.Default.SavedId;
+                chkSaveId.IsChecked = true;
+            }
+
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Button_Click_1(object sender, RoutedEventArgs e)
         {
+            // 아이디 저장 로직
+            if (chkSaveId.IsChecked == true)
+            {
+                Properties.Settings.Default.SavedId = txtId.Text;
+                Properties.Settings.Default.IsSaveId = true;
+            }
+            else
+            {
+                Properties.Settings.Default.SavedId = "";
+                Properties.Settings.Default.IsSaveId = false;
+            }
+
+            Properties.Settings.Default.Save();
+
+            // 로그인 로직
             string id = txtId.Text.Trim();
             string pw = txtPw.Password.Trim();
             if (string.IsNullOrEmpty(id) || string.IsNullOrEmpty(pw))
@@ -53,6 +76,9 @@ namespace campusCore
 
                     if (count == 1)
                     {
+                        // 세션에 로그인한 학생 아이디 저장
+                        UserSession.StudentId = id;
+
                         MainWindow main = new MainWindow();
                         main.Show();
                         this.Close();
@@ -64,5 +90,11 @@ namespace campusCore
                 }
             }
         }
+        private void txtId_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            phId.Visibility = string.IsNullOrEmpty(txtId.Text) ? Visibility.Visible : Visibility.Hidden;
+        }
+    
     }
+   
 }
